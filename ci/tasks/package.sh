34859b182555a9944e925e2a99de892886a5cd8c
Bruce E. Thelen <bthelen@pivotal.io>
2018-06-25 17:21:01 -0500

    Removing the CI directory, that's where the workshop participants work would go.

--- ci/tasks/package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-cd git-assets/
-mvn -version
-mvn help:active-profiles
-mvn package
-cp target/concourse-demo-*.jar ../app-output/concourse-demo.jar
--- ci/tasks/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-cd git-assets
-mvn -version
-mvn help:active-profiles
-mvn test
